2bfb14a471f2d1677be3bd5542b7cd0443e4f700
Derek Houck <derek@derekhouck.com>
2019-01-22 09:29:26 -0800

    Add NavigationBar and GameBoard components

---
+++ src/components/GameBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function GameBoard (props) {
+  return (
+    <section>
+      {/* Hinter */}
+      {/* Guess Form */}
+      {/* Guess Log */}
+    </section>
+  );
+}
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavigationBar.css';
+
+export default function NavigationBar(props) {
+  return (
+    <nav>
+      <ul>
+        <li>WHAT?</li>
+        <li>+ NEW GAME</li>
+      </ul>
+    </nav>
+  );
+}
